dcd43060e4e99fafab75dd8afd2803f68e14693a
henryc1008 <80172761+henryc1008@users.noreply.github.com>
2025-01-14 16:56:26 -0800

    Update Navbar.svelte

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -4,12 +4,12 @@
 <div>
     hi from navbar
     <nav>
-        <a href="/about" > about</a>
-        <a href="/contact" > contact</a>
-        <a href="/#" > home</a>
-        <a href="/animals/Animal1">Animal 1 </a>
-        <a href="/animals/Animal2">Animal 2 </a>
-        <a href="/animals/Animal3">Animal 3 </a>
+        <a href="{page}/about" > about</a>
+        <a href="{page}/contact" > contact</a>
+        <a href="{page}/#" > home</a>
+        <a href="{page}/animals/Animal1">Animal 1 </a>
+        <a href="{page}/animals/Animal2">Animal 2 </a>
+        <a href="{page}/animals/Animal3">Animal 3 </a>
     </nav>
 </div>
 <style lang="scss">
