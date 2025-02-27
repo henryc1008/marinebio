638da0db98cd70e1de2c49b6b9cf6492151fe18d
henryc1008 <80172761+henryc1008@users.noreply.github.com>
2025-02-26 17:03:29 -0800

    Update +page.svelte

--- src/routes/animals/+page.svelte
+++ src/routes/animals/+page.svelte
@@ -1,11 +1,12 @@
 <script>
     let {data} =$props();
+    import {base} from "$app/paths";
 </script>
 
 <h1> Animals</h1>
 
 <ul>
     {#each data.summaries as {slug, title}}
-    <li><a href="/animals/{slug}">{title}</a></li>
+    <li><a href="{base}/animals/{slug}">{title}</a></li>
     {/each}
 </ul>
